540177b70c44847f29c3bc7d94e07effa628f2d8
soyou088 박성우 <soyou088@gmail.com>
2024-04-09 18:17:20 +0900

    박성우 : 맵 9장 업데이트

--- Contents/PlayGameMode.cpp
+++ Contents/PlayGameMode.cpp
@@ -16,7 +16,7 @@ void APlayGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UContentsConstValue::MapTex = UEngineTexture::FindRes("Holo_map_06.png");
+	UContentsConstValue::MapTex = UEngineTexture::FindRes("Holo_map_04.png");
 	UContentsConstValue::MapTexScale = UContentsConstValue::MapTex->GetScale();
 
 	std::shared_ptr<UCamera> Camera = GetWorld()->GetMainCamera();
@@ -34,16 +34,39 @@ void APlayGameMode::BeginPlay()
 		Actor->SetActorLocation({ ImageScale.hX(), -ImageScale.hY(), 1000.0f });
 	}
 
-	{
-		std::shared_ptr<APlayBack> Back = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back0 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back1 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back2 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back3 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back4 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back5 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back6 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back7 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+		std::shared_ptr<APlayBack> Back8 = GetWorld()->SpawnActor<APlayBack>("PlayBack");
+
 
 		float TileSize = UContentsConstValue::TileSize;
 		float4 TexScale = UContentsConstValue::MapTexScale;
 		float4 ImageScale = { TexScale.X * TileSize, TexScale.Y * TileSize, 0.0f };
 
-		Back->SetActorScale3D(ImageScale);
-		Back->SetActorLocation({ ImageScale.hX(), -ImageScale.hY(), 500.0f });
-	}
+		Back0->SetActorScale3D(ImageScale);
+		Back0->SetActorLocation({ ImageScale.hX(), -ImageScale.hY(), 500.0f });             // 가운데
+		Back1->SetActorScale3D(ImageScale);
+		Back1->SetActorLocation({ ImageScale.hX(), -ImageScale.hY() * 3, 500.0f });         // 아래
+		Back2->SetActorScale3D(ImageScale);
+		Back2->SetActorLocation({ ImageScale.hX() * 3, -ImageScale.hY(), 500.0f });         // 오른쪽
+		Back3->SetActorScale3D(ImageScale);
+		Back3->SetActorLocation({ ImageScale.hX() * 3, -ImageScale.hY() * 3, 500.0f });     // 오른쪽 대각선 아래
+		Back4->SetActorScale3D(ImageScale);
+		Back4->SetActorLocation({ ImageScale.hX() * 3, ImageScale.hY(), 500.0f });          // 오른쪽 대각선 위
+		Back5->SetActorScale3D(ImageScale);
+		Back5->SetActorLocation({ -ImageScale.hX() , ImageScale.hY(), 500.0f });            // 왼쪽 대각선 위
+		Back6->SetActorScale3D(ImageScale);
+		Back6->SetActorLocation({ -ImageScale.hX() , -ImageScale.hY() * 3, 500.0f });       // 왼쪽 대각선 아래
+		Back7->SetActorScale3D(ImageScale);
+		Back7->SetActorLocation({ -ImageScale.hX() , -ImageScale.hY(), 500.0f });           // 왼쪽
+		Back8->SetActorScale3D(ImageScale);
+		Back8->SetActorLocation({ ImageScale.hX() , ImageScale.hY(), 500.0f });            // 위쪽
 }
 
 
@@ -51,8 +74,6 @@ void APlayGameMode::BeginPlay()
 void APlayGameMode::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
-
-	
 
 }
 
--- Contents/PlayGameMode.h
+++ Contents/PlayGameMode.h
@@ -23,6 +23,7 @@ protected:
 
 
 private:
+	
 
 };
 
